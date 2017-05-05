a70aa7c77432d9beef221ae5cdd5204aece6fa24
Noman Rauf <nomi589@hotmail.com>
2017-05-05 20:08:25 +0300

    Add slug column to users table and use slugs instead of user ids

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,29 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }, format: { without: /\s/, message: "can't have spaces" }
   validates :email, presence: true, uniqueness: true
+
+  before_save :generate_slug
+
+  def generate_slug
+    slug = self.username.downcase.parameterize
+
+    if User.exists?(slug: slug)
+      postfix = 1
+
+      loop do
+        slug = "#{slug}-#{postfix}"
+        break unless User.exists?(slug: slug)
+        postfix += 1
+        slug = self.username.downcase.parameterize
+      end
+    end
+
+    self.slug = slug
+  end
+
+  def to_param
+    self.slug
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503231911) do
+ActiveRecord::Schema.define(version: 20170505145308) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170503231911) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
 end
