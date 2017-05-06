bae2bc91d12d7ad493ac2950e7892a3b1c95a4c0
Noman Rauf <nomi589@hotmail.com>
2017-05-06 12:25:34 +0300

    Fixed a bug is sessions_controller.rb that made login impossible

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
-  before_action :require_login, except: [:new]
-  before_action :require_logout, only: [:new]
+  before_action :require_login, only: [:show, :destroy]
+  before_action :require_logout, except: [:show, :destroy]
 
   def new
 
